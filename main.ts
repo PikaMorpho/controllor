096d7ff20cc38e4bc5f19882a2eea3322b97df03
PikaMorpho <526998@bsd48.org>
2025-05-07 14:00:28 -0700

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,35 +1,45 @@
-radio.setGroup(1)  // Set the radio group to 1 for communication with the robot
-
+// Set the radio group to 1 for communication with the robot
 // Move forward when Button A is pressed
 input.onButtonPressed(Button.A, function () {
-    radio.sendString("forward")  // Send "forward" command to the robot
-    basic.showArrow(ArrowNames.North)  // Show an upward arrow indicating forward movement
+    // Send "forward" command to the robot
+    radio.sendString("forward")
+    // Show an upward arrow indicating forward movement
+    basic.showArrow(ArrowNames.North)
 })
-
-// Move backward when Button B is pressed
-input.onButtonPressed(Button.B, function () {
-    radio.sendString("backward")  // Send "backward" command to the robot
-    basic.showArrow(ArrowNames.South)  // Show a downward arrow indicating backward movement
-})
-
 // Turn left when Button A + B is pressed
 input.onButtonPressed(Button.AB, function () {
-    radio.sendString("left")  // Send "left" command to the robot
-    basic.showArrow(ArrowNames.West)  // Show a leftward arrow indicating left turn
+    // Send "left" command to the robot
+    radio.sendString("left")
+    // Show a leftward arrow indicating left turn
+    basic.showArrow(ArrowNames.West)
 })
-
+// Move backward when Button B is pressed
+input.onButtonPressed(Button.B, function () {
+    // Send "backward" command to the robot
+    radio.sendString("backward")
+    // Show a downward arrow indicating backward movement
+    basic.showArrow(ArrowNames.South)
+})
 // Turn right when tilting right
 input.onGesture(Gesture.TiltRight, function () {
-    radio.sendString("right")  // Send "right" command to the robot
-    basic.showArrow(ArrowNames.East)  // Show a rightward arrow indicating right turn
+    // Send "right" command to the robot
+    radio.sendString("right")
+    // Show a rightward arrow indicating right turn
+    basic.showArrow(ArrowNames.East)
 })
-
 // Stop movement when the logo is pressed
 input.onLogoEvent(TouchButtonEvent.Pressed, function () {
-    radio.sendString("stop")  // Send "stop" command to the robot
-    basic.showIcon(IconNames.No)  // Show "No" icon indicating stopping
+    // Send "stop" command to the robot
+    radio.sendString("stop")
+    // Show "No" icon indicating stopping
+    basic.showIcon(IconNames.No)
 })
-
-// Display a happy face when the controller is ready
-basic.showIcon(IconNames.Happy)
-
+// Set the radio group to 1 for communication with the robot
+radio.setGroup(1)
+basic.showLeds(`
+    . . . . .
+    . # . # .
+    . # . # .
+    # . . . #
+    . # # # .
+    `)
